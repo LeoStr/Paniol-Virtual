b472c1854be1e73093c897d8e2b4b3f0beb278d2
Leo <leos_ar@hotmail.com>
2017-04-11 19:12:13 -0300

    creamos el user de guille y comentamos de nuevo

--- grails-app/init/paniolvirtual/BootStrap.groovy
+++ grails-app/init/paniolvirtual/BootStrap.groovy
@@ -6,18 +6,18 @@ import paniolvirtual.UserRole
 
 class BootStrap {
     def init = { servletContext ->
-        /*  //Armamos un primer usuario
+        /*  /Armamos un primer usuario
         def adminRole = new Role(authority: 'ROLE_ADMIN').save()
         def userRole = new Role(authority: 'ROLE_USER').save()
-        def testUser = new User(username: 'straguzzi', password: 'micasa', nombre: 'Leonardo', apellido: 'Straguzzi', direccion: 'Av de Mayo 1316 p 3 dto D').save(flush:true, failOnError: true, insert: true)
+        def testUser = new User(username: 'quinteros', password: 'guille', nombre: 'Guillermo', apellido: 'Quinteros', direccion: 'No me acuerdo tu direccion').save(flush:true, failOnError: true, insert: true)
         UserRole.create testUser, adminRole
         UserRole.withSession {
             it.flush()
             it.clear()
         }
-        assert User.count() == 1
+        assert User.count() == 2
         assert Role.count() == 2
-        assert UserRole.count() == 1
+        assert UserRole.count() == 2
         */
     }
 
