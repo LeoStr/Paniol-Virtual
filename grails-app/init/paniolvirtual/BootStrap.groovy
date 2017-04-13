a8a703ed32a20aefbc650c68267c18cb63a9fba4
Leo <leos_ar@hotmail.com>
2017-04-12 22:34:39 -0300

    Creamos un usuario user y volvemos a comentar

--- grails-app/init/paniolvirtual/BootStrap.groovy
+++ grails-app/init/paniolvirtual/BootStrap.groovy
@@ -6,18 +6,19 @@ import paniolvirtual.UserRole
 
 class BootStrap {
     def init = { servletContext ->
-        /*  /Armamos un primer usuario
-        def adminRole = new Role(authority: 'ROLE_ADMIN').save()
-        def userRole = new Role(authority: 'ROLE_USER').save()
-        def testUser = new User(username: 'quinteros', password: 'guille', nombre: 'Guillermo', apellido: 'Quinteros', direccion: 'No me acuerdo tu direccion').save(flush:true, failOnError: true, insert: true)
-        UserRole.create testUser, adminRole
+        /* //Armamos un usuario
+        //def adminRole = new Role(authority: 'ROLE_ADMIN').save()
+        //def userRole = new Role(authority: 'ROLE_USER').save()
+        def role = Role.findByAuthority('ROLE_USER')
+        def testUser = new User(username: 'cliente', password: 'cliente', nombre: 'Cliente', apellido: 'Cliente', direccion: 'Donde sea').save(flush:true, failOnError: true, insert: true)
+        UserRole.create testUser, role
         UserRole.withSession {
             it.flush()
             it.clear()
         }
-        assert User.count() == 2
-        assert Role.count() == 2
-        assert UserRole.count() == 2
+        //assert User.count() == 2
+        //assert Role.count() == 2
+        //assert UserRole.count() == 2
         */
     }
 
